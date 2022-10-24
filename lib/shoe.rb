eaedcdb59a228506db524c9fb6d05de680654939
+ <+>
2022-10-24 13:48:39 +0300

    Done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,20 @@
 # Make your shoe class here!
-
+require 'pry'
 class Shoe
 
     attr_accessor :brand, :color, :size, :material, :condition
 
-    def initialize(brand)
+    def initialize(brand = "Tom Ford", color = 'black', size = 10, material = 'leather', condition = "old" )
         @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+
     end
     def cobble
         puts "Your shoe is as good as new!"
         @condition = "new"
     end
 end
+binding.pry
